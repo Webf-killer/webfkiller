5bde37da30525eb6640947bdd111620c82f9bff2
ggugga <rkrud616@naver.com>
2023-11-22 15:44:45 +0900

    최종은 아님 그럴리가

--- attack.py
+++ attack.py
@@ -4,8 +4,9 @@ from urllib.parse import urlencode
 
 class AttackModule:
     def get_data_for_attack_type(self, table):
+        column = 'payload' if 'payloads' in table else 'url' if table == 'urls' else 'data'
         try:
-            self.mycursor.execute(f"SELECT data FROM {table}")
+            self.mycursor.execute(f"SELECT {column} FROM {table}")
             results = self.mycursor.fetchall()
             return [row[0] for row in results]
         except mysql.connector.Error as err:
@@ -19,12 +20,16 @@ class AttackModule:
         except mysql.connector.Error as err:
             print(f"Error: {err}")
             return
+        
+        self.sql_errors = self.get_data_for_attack_type('sql_errors')
+        self.xss_errors = self.get_data_for_attack_type('xss_errors')
+        self.or_errors = self.get_data_for_attack_type('or_errors')
 
-        tables_attack_types = [('payloads_sqli', 'sqli'), ('payloads_xss', 'xss'), ('payloads_or', 'openredir')]
-        self.payloads = {attack_type: self.fetch_payloads(table) for table, attack_type in tables_attack_types}
+        tables_attack_types = [('payloads_sqli', 'sqli'), ('payloads_xss', 'xss'), ('payloads_or', 'or')]
+        self.payloads = {attack_type: self.get_data_for_attack_type(table) for table, attack_type in tables_attack_types}
 
-        self.urls = self.fetch_urls()
-        self.data = {vuln: self.get_data_for_attack_type(f'{vuln}_data') for vuln in ['sqli', 'xss', 'openredir']}
+        self.urls = self.get_data_for_attack_type('urls')
+        self.data = {vuln: self.get_data_for_attack_type(f'{vuln}_data') if vuln != 'or' else self.get_data_for_attack_type('or_data') for vuln in ['or']}
 
         methods = ['GET', 'POST', 'PUT']
         for method in methods:
@@ -36,41 +41,23 @@ class AttackModule:
                         for payload in self.payloads[vuln]:
                             self.send_request(method, url, data, payload, vuln)
 
-    def fetch_payloads(self, table):
-        try:
-            self.mycursor.execute(f"SELECT payload FROM {table}")
-            results = self.mycursor.fetchall()
-            return [row[0] for row in results]
-        except mysql.connector.Error as err:
-            print(f"Error: {err}")
-            return []
-
-    def fetch_urls(self):
-        try:
-            self.mycursor.execute("SELECT url FROM urls")
-            results = self.mycursor.fetchall()
-            return [row[0].strip("'") for row in results]
-        except mysql.connector.Error as err:
-            print(f"Error: {err}")
-            return []
-
-    def send_request(self, method, url, data, payload, vuln):
+    def send_request(self, method, url, data_key, payload, vuln):
         if method not in ['GET', 'POST', 'PUT']:
             print(f"Invalid method: {method}")
             return
 
         try:
             if method == 'GET':
-                full_url = f"{url}?{urlencode({data: payload})}"
+                full_url = f"{url}?{urlencode({data_key: payload})}"
                 print(f"Sending {method} request to: {full_url}")
                 response = requests.request(method, full_url)
             else:  # POST or PUT
-                data = {data: payload}
-                print(f"Sending {method} request to: {url}")
+                data = {data_key: payload}
+                print(f"Sending {method} request to: {url} with data: {data}")
                 headers = {'Content-Type': 'application/json'}
                 response = requests.request(method, url, json=data, headers=headers)
 
-            self.analyze_response(url, data, payload, response, vuln)
+            self.analyze_response(url, data_key, payload, response, vuln)
         except requests.exceptions.RequestException as e:
             print(f"{method} request failed: {e}")
 
@@ -79,26 +66,29 @@ class AttackModule:
             self.log_success(url, data, payload, response.status_code, vuln)
         elif vuln == 'xss' and self.is_xss(response, payload):
             self.log_success(url, data, payload, response.status_code, vuln)
-        elif vuln == 'openredir' and self.is_openredir(response, url):
+        elif vuln == 'or' and self.is_or(response, url):
             self.log_success(url, data, payload, response.status_code, vuln)
         else:
             print("Attack failed")
 
     def is_sqli(self, response, payload):
-        return response.status_code == 200 and payload in response.text
+        return response.status_code == 200 or any(error in response.text for error in self.sql_errors)
 
     def is_xss(self, response, payload):
-        return response.status_code == 200 and payload in response.text
+        return response.status_code == 200 or any(error in response.text for error in self.xss_errors)
 
-    def is_openredir(self, response, url):
-        return response.status_code in [301, 302] and response.headers.get('Location', '') == url
+    def is_or(self, response, url):
+        return response.status_code in [301, 302] and any(error in response.text for error in self.or_errors)
 
-    def log_success(self, url, data, payload, status_code, vuln):
+    def log_success(self, url, data_key, payload, status_code, vuln):
         print("Attack might have been successful")
+        self.mycursor.execute(f"TRUNCATE TABLE success_{vuln}")  # 테이블 초기화
+        self.attackDB.commit()  # 변경 사항 적용
         self.mycursor.execute(f"CREATE TABLE IF NOT EXISTS success_{vuln} (id INT AUTO_INCREMENT, success LONGTEXT, PRIMARY KEY (id))")
         self.mycursor.execute(f"INSERT INTO success_{vuln} (success) VALUES (%s)",
-                            (f"URL: {url}, Data: {data}, Payload: {payload}, Status Code: {status_code}, Vuln: {vuln}",))
+                            (f"URL: {url}, Data: {data_key}, Payload: {payload}, Status Code: {status_code}, Vuln: {vuln}",))
         self.attackDB.commit()
 
 attack_module = AttackModule()
+
 
--- attackDB.py
+++ attackDB.py
@@ -10,9 +10,13 @@ tables_files = [('urls', 'url', 'urls_data.txt'), ('xss_data', 'data', 'xss_data
 
 # 각 테이블에 대해
 for table, column, file in tables_files:
+    # 테이블의 기존 데이터 삭제
+    mycursor.execute(f"TRUNCATE TABLE {table}")
+    attackDB.commit()  # 변경 사항 적용
+
     # 해당 파일 읽어오기
     with open(file, 'r') as f:
-        data = f.read().split('\n')  
+        data = f.read().splitlines()  
 
    
     # 테이블에 데이터 삽입
@@ -22,8 +26,7 @@ for table, column, file in tables_files:
 
     attackDB.commit()  # 각 테이블에 대해 데이터 삽입이 끝날 때마다 commit 호출 
 
-    #record_success 테이블을 만들어 공격분석후 저장하도록 해야함
-    #attack 모듈로 부터 받은 success_xss, success_sqli, success_o.r 
+
    
 
 
