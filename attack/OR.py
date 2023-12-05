7a5d9cdec7dd0f1e46a50c1f8082aab82e893b07
kimmnzy <mnzy412@gmail.com>
2023-12-05 20:39:58 +0900

    피드백 반영

--- attack/OR.py
+++ attack/OR.py
@@ -1,32 +1,69 @@
 import requests
+from bs4 import BeautifulSoup
+import urllib.parse
+import re
 
-def test_or(self, url, domain):  # Attack 객체를 첫 번째 인자로 받음+ name = 요청한 페이지의 url 도메인 
-    #main에서 생성된 WebDriver 객체를 이용하여 페이지 이동 (redirect 페이지로 이동)
-    self.driver.get(url)
+class OpenRedirection: 
 
-    # 페이지를 완전히 로드한 후의 상태를 확인 (클라이언트 측 리다이렉트 감지를 위함)
-    #리다이렉트 이후의 response 가져와서 url 가져오기
-    redirect_url = self.driver.current_url
+    def __init__(self, driver):  
+        self.driver = driver 
 
-    # 만약 현재 URL이 처음에 요청한 URL과 다르고, '/'로 시작하지 않고 주어진 'name'으로 시작하지 않는 경우 (=다른 도메인으로 이동하는 경우)
-    if  redirect_url != url and not url.startswith('/') and not url.startswith(domain):
-        # Open redirection이 감지된 경우
-        print("Open redirection was detected")
-    else:
-        # Open redirection이 아닌 경우
-        print("No open redirection")
+    def test_or(self, request, response):
+        url = request.url
+        domain = urllib.parse.urlparse(url).netloc
 
-    response = requests.get(url, allow_redirects=True)
-
-    # 만약 응답의 상태 코드가 리다이렉트 코드인 경우(301, 302, 303, 307, 308)
-    if response.status_code in [301, 302, 303, 307, 308]:
-        # 응답 헤더에서 'Location'을 가져와 리다이렉트된 URL을 얻음
-        redirect_header = response.headers.get('Location')
-
-        # 만약 리다이렉트된 URL이 존재하고, '/'로 시작하지 않고 주어진 'name'으로 시작하지 않는 경우
-        if redirect_header and not redirect_header.startswith('/') and not redirect_url.startswith(domain):
-            # Open redirection이 감지된 경우
-            print("Open redirection was detected")
+        # WebDriver를 이용하여 리다이렉트 탐지
+        self.driver.get(url)
+        redirect_url = self.driver.current_url
+        #웹 드라이버가 현재 접근한 URL (self.driver.current_url)과 처음에 요청한 URL (url)이 동일한지 비교하여 리다이렉트를 감지
+        #상대 경로로 시작하는지 (redirect_url.startswith('/'))도 확인
+        #리다이렉트가 동일한 도메인 내부나 동일한 웹 페이지 내부로 이루어진 것이 아닌지를 확인
+        if  redirect_url != url and not redirect_url.startswith('/') and not redirect_url.startswith(domain):
+            print("WebDriver-based open redirection was detected")
         else:
-            # Open redirection이 아닌 경우
-            print("No open redirection")
+            print("No WebDriver-based open redirection")
+
+
+
+        # 클라이언트 측 리다이렉트 탐지
+        
+        redirect_url = response.url
+        if  redirect_url != url and not redirect_url.startswith('/') and not redirect_url.startswith(domain):
+            print("Client-side open redirection was detected")
+        else:
+            print("No client-side open redirection")
+
+        # 추가한 부분
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # HTML meta 태그를 사용하는 리다이렉트 감지
+        # HTML 문서에서 <meta http-equiv="refresh" content="..."> 형태의 태그를 찾아서 리다이렉트를 감지
+        # 웹 페이지를 새로 고침하거나 다른 페이지로 리다이렉트시키는 역할
+        meta_refresh = soup.find('meta', attrs={'http-equiv': re.compile("^refresh$", re.I)})
+        if meta_refresh and 'content' in meta_refresh.attrs:
+            print("Client-side redirection (via meta tag) detected. Content:", meta_refresh['content'])
+
+        # JavaScript를 사용하는 리다이렉트 감지
+        # window.location 객체를 사용하여 리다이렉트를 수행하는 경우
+        scripts = soup.find_all('script')
+        for script in scripts:
+            if 'window.location' in script.text:
+                print("Client-side redirection (via JavaScript) detected in script:", script.text)
+
+
+        # 서버 측 리다이렉트 확인 (수정)
+        server_side_redirect_detected = False
+
+        # HTTP 상태 코드 3xx와 함께 'Location' 헤더를 포함하는지 확인
+        if response.status_code in [301, 302, 303, 307, 308] and 'Location' in response.headers:
+            print("Server-side redirection detected to:", response.headers['Location'])
+            server_side_redirect_detected = True
+
+        # 'Refresh' 헤더를 사용하는 리다이렉트 감지
+        if 'Refresh' in response.headers:
+            print("Server-side redirection (via Refresh header) detected. Content:", response.headers['Refresh'])
+            server_side_redirect_detected = True
+
+        if not server_side_redirect_detected:
+            print("No server-side redirection was detected")
+
--- attack/main.py
+++ attack/main.py
@@ -6,18 +6,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from urllib.parse import urlparse #OR_도메인 정보 가져올 때 필요
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
 import time
-import DomXSS, StoredXSS, ReflectedXSS, OR, SQLi
+import DomXSS
 import re
 import socket
-from importlib import reload
-import DomXSS
-reload(DomXSS)
 from DomXSS import DomXSS
-from OR import test_or
+from OR import OpenRedirection
 
 class Attack:
     ATTACK_TYPE_DOM_BASED_XSS = 'Dom_based_xss'
@@ -46,7 +42,7 @@ class Attack:
         
     def connect_db(self):
         try:
-            return mysql.connector.connect(host="localhost", user="root", password="root", database="attackDB")
+            return mysql.connector.connect(host="localhost", user="root", password="1111", database="attackDB")
         except mysql.connector.Error as err:
             print(f"Failed to connect to database: {err}")
             return None
@@ -67,6 +63,7 @@ class Attack:
         self.driver.get("http://sekurity.online:8080/login.php")  # DVWA 페이지를 바로 연결
         self.payloads = self.get_data('payloads_DOMXss', 'payload')
         self.dom_xss_detector = DomXSS(self.driver, re.compile(r'<script>|</script>|javascript:|onload=|onerror='), self.payloads, self.mycursor, self.attackDB)
+        self.open_redirection_detector = OpenRedirection(self.driver)
 
         # DVWA 로그인
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "username")))
@@ -114,19 +111,21 @@ class Attack:
     def process_url(self, url):
         print(f"Sending GET request to {url} with proxies {self.proxies}")  # GET 요청 보내는 것을 출력
         try:
+            # 요청 객체 생성
+            request = requests.Request('GET', url).prepare()
+
+            # GET 요청 보내기
             response = requests.get(url, timeout=5)
+
+            # DOM-based XSS에 대한 공격을 수행
             self.dom_xss_detector.test_dom_based_xss(url)
+            
+            # Open Redirection에 대한 공격을 수행
+            self.open_redirection_detector.test_or(request, response)
+            
             print(f"Response status code: {response.status_code}")  # 응답의 상태 코드 출력
-        
         except Exception as e:
             print(f"Failed to send GET request: {e}")
-
-        
-    def process_or(self, current_url):
-        #current_url : 현재 url가져오기 
-        domain = urlparse(current_url).netloc #현재 url의 도메인 
-        test_or(self, current_url, domain) #url과 도메인 가져가기 
-       
 
 if __name__ == "__main__":
     attack = Attack()
